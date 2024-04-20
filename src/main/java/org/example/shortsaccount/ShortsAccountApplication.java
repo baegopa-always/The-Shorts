30210b8019fd3b0791bf5e23bf905fc5f7813e06
tony <fjzl12@naver.com>
2024-04-20 20:55:19 +0900

    init

---
+++ src/main/java/org/example/shortsaccount/ShortsAccountApplication.java
@@ -0,0 +1,13 @@
+package org.example.shortsaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortsAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortsAccountApplication.class, args);
+    }
+
+}
