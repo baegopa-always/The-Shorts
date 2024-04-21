55d3b1353775d7ba757fb07c0cd78fe227cc94fc
tony <fjzl12@naver.com>
2024-04-22 01:43:28 +0900

    feat(entity): create token/user entity

---
+++ src/main/java/org/example/shortsaccount/domain/User.java
@@ -0,0 +1,87 @@
+package org.example.shortsaccount.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", updatable = false)
+    private Long memberId;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "role_id", nullable = false)
+    private int role_id;
+
+    @Column(name = "social_id", nullable = false)
+    private int social_id;
+
+    @Column(name = "social_provider", nullable = false)
+    private String social_provider;
+
+    @Builder
+    public User(String email, String username, int role_id, String social_provider, int social_id, String auth) {
+        this.email = email;
+        this.username = username;
+        this.role_id = role_id;
+        this.social_provider = social_provider;
+        this.social_id = social_id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    public Long getId() {
+        return memberId;
+    }
+}
