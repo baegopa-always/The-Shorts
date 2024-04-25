af443c4c6fc7ec302106005f6132e31eb97924ab
tony <fjzl12@naver.com>
2024-04-25 22:01:35 +0900

    refactor(VideoResponse): 영상 반환 값 수정

--- src/main/java/org/example/shortsaccount/dto/VideoResponse.java
+++ src/main/java/org/example/shortsaccount/dto/VideoResponse.java
@@ -7,12 +7,21 @@ import java.time.LocalDateTime;
 
 @Getter
 public class VideoResponse {
+    private final long videoId;
     private final String title;
     private final int length;
     private final LocalDateTime uploadDate;
+    private final int memberId;
+    private final int playbackTime;
+    private final int videoViews;
+
     public VideoResponse(Video video) {
+        this.videoId = video.getVideoId();
         this.title = video.getTitle();
         this.length = video.getLength();
         this.uploadDate = video.getUploadDate();
+        this.memberId = video.getMemberId();
+        this.playbackTime = video.getPlaybackTime();
+        this.videoViews = video.getVideoViews();
     }
 }
