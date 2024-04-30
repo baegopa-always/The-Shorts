cf65b47d3404646dc57cff32acd2407ef9f31961
tony <fjzl12@naver.com>
2024-04-30 10:55:12 +0900

    refactor(PlayHistory): 재생 기록 도메인 수정 UserHistory 도메인과 병합

--- src/main/java/org/example/shortsaccount/domain/PlayHistory.java
+++ src/main/java/org/example/shortsaccount/domain/PlayHistory.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
-@Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
 public class PlayHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,12 +22,25 @@ public class PlayHistory {
     @Column(name = "video_id", nullable = false)
     private int videoId;
 
-    @Column(name = "play_date", columnDefinition = "TIMESTAMP")
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "last_watch_time", nullable = false)
+    private int lastWatchTime;
+
+    @Column(name = "play_date", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime playDate;
 
     @Builder
-    public PlayHistory(int videoId) {
+    public PlayHistory(int videoId, int userId, int lastWatchTime){
         this.videoId = videoId;
+        this.userId = userId;
+        this.lastWatchTime = lastWatchTime;
         this.playDate = LocalDateTime.now();
     }
+
+    public void update(int lastWatchTime) {
+        this.lastWatchTime = lastWatchTime;
+    }
+
 }
