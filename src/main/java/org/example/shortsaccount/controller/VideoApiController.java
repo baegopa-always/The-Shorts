ae420d4723a5ef0974c54129e66b9a2e1388bb05
tony <fjzl12@naver.com>
2024-04-30 11:05:49 +0900

    refactor(VideoApiController): playHistory 수정 userHistory -> playHistory 통합

--- src/main/java/org/example/shortsaccount/controller/VideoApiController.java
+++ src/main/java/org/example/shortsaccount/controller/VideoApiController.java
@@ -2,13 +2,12 @@ package org.example.shortsaccount.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.shortsaccount.domain.PlayHistory;
-import org.example.shortsaccount.domain.UserHistory;
 import org.example.shortsaccount.domain.Video;
 import org.example.shortsaccount.dto.PlayHistoryDTO;
 import org.example.shortsaccount.dto.VideoDTO;
 import org.example.shortsaccount.dto.VideoResponse;
 import org.example.shortsaccount.service.PlayHistoryService;
-import org.example.shortsaccount.service.UserHistoryService;
+import org.example.shortsaccount.service.VideoAdvertisementService;
 import org.example.shortsaccount.service.VideoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +20,7 @@ import java.util.List;
 public class VideoApiController {
     private final VideoService videoService;
     private final PlayHistoryService playHistoryService;
-    private final UserHistoryService userHistoryService;
+    private final VideoAdvertisementService videoAdvertisementService;
 
     @PostMapping("/api/videos")
     public ResponseEntity<Video> addVideo(@RequestBody VideoDTO request) {
@@ -31,32 +30,32 @@ public class VideoApiController {
     }
 
     @PostMapping("/api/videos/play/{id}")
-    public ResponseEntity<UserHistory> playVideo(@PathVariable int id, @RequestBody PlayHistoryDTO request) {
+    public ResponseEntity<PlayHistory> playVideo(@PathVariable int id, @RequestBody PlayHistoryDTO request) {
+        request.setVideoId(id);
         // video id에 해당하는 video_views count 증가
         videoService.checkVideo(id);
         // body 에서 user 정보 가져와서 사용자 재생 기록 확인
-        UserHistory userHistory;
+        PlayHistory playHistory;
         try {
-            userHistory = userHistoryService.findVideoHistoryByUserId(request.getMemberId(), id);
+            playHistory = playHistoryService.findVideoHistoryByUserId(request.getMemberId(), id);
         } catch (IllegalArgumentException e) {
-            userHistory = userHistoryService.save(id, request);
+            playHistory = playHistoryService.save(request);
             // 0으로 보내주자
         }
         // 재생 시점 있을 경우 불러와서 재생
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(userHistory);
+                .body(playHistory);
     }
 
     @PostMapping("/api/videos/stop/{id}")
     public ResponseEntity<PlayHistory> stopVideo(@PathVariable int id, @RequestBody PlayHistoryDTO request) {
         // play history 생성
         request.setVideoId(id);
-        PlayHistory playHistory = playHistoryService.save(request);
         // 해당 video id에서 재생시간만큼 playback time 증가, ad_views 증가
-        videoService.addPlayTime(id, request.getPlayTime());
-        videoService.addAdViews(id, request.getPlayTime());
+        videoService.addPlayTime(request.getVideoId(), request.getPlayTime());
+        videoAdvertisementService.checkVideoAdvertisement(id, request.getPlayTime());
         // 사용자 기록 업데이트
-        userHistoryService.updateLastWatchTime(id, request);
+        PlayHistory playHistory = playHistoryService.updateLastWatchTime(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(playHistory);
     }
