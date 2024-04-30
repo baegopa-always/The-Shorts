00b0ac0bb92b7cb4c5bf805c89ad4955e2a8cf1c
tony <fjzl12@naver.com>
2024-04-30 10:58:27 +0900

    feat(PlayHistoryService): 재생 기록 서비스 수정 - 사용자 재생 기록 있을 경우 재생 시점 불러오는 기능 구현 - 영상 정지 시점 저장 기능 구현

--- src/main/java/org/example/shortsaccount/service/PlayHistoryService.java
+++ src/main/java/org/example/shortsaccount/service/PlayHistoryService.java
@@ -5,6 +5,7 @@ import org.example.shortsaccount.domain.PlayHistory;
 import org.example.shortsaccount.dto.PlayHistoryDTO;
 import org.example.shortsaccount.repository.PlayHistoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +15,26 @@ public class PlayHistoryService {
     public PlayHistory save(PlayHistoryDTO request) {
         return playHistoryRepository.save(PlayHistory.builder()
                 .videoId(request.getVideoId())
+                .userId(request.getMemberId())
+                .lastWatchTime(request.getPlayTime())
                 .build());
+    }
+
+    @Transactional
+    public PlayHistory updateLastWatchTime(PlayHistoryDTO request) {
+        PlayHistory playHistory = playHistoryRepository.findByUserIdAndVideoId(request.getMemberId(), request.getVideoId())
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + request.getVideoId()));
+        playHistory.update(request.getPlayTime());
+        return playHistory;
+    }
+
+    public PlayHistory findAllVideoHistoryByUserId(long userId) {
+        return playHistoryRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + userId));
+    }
+
+    public PlayHistory findVideoHistoryByUserId(int userId, int videoId) {
+        return playHistoryRepository.findByUserIdAndVideoId(userId, videoId)
+                .orElseThrow(() -> new IllegalArgumentException("not found user: " + userId + " video: "+ videoId));
     }
 }
