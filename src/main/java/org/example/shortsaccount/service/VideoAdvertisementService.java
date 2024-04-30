835cf20a924af051d9f357799e83be590e85cb3e
tony <fjzl12@naver.com>
2024-04-30 11:04:17 +0900

    feat(VideoAdvertisementService): 영상 내 광고 서비스 구현 - 광고 본 횟수 만큼 카운트하는 기능 구현

---
+++ src/main/java/org/example/shortsaccount/service/VideoAdvertisementService.java
@@ -0,0 +1,35 @@
+package org.example.shortsaccount.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.shortsaccount.domain.Video;
+import org.example.shortsaccount.domain.VideoAdvertisement;
+import org.example.shortsaccount.repository.AdvertisementRepository;
+import org.example.shortsaccount.repository.VideoAdvertisementRepository;
+import org.example.shortsaccount.repository.VideoRepository;
+import org.example.shortsaccount.util.RandomNumberGenerator;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class VideoAdvertisementService {
+    private final VideoAdvertisementRepository videoAdvertisementRepository;
+    private final AdvertisementRepository advertisementRepository;
+    private final VideoRepository videoRepository;
+
+    @Transactional
+    public void checkVideoAdvertisement(int videoId, int playtime) {
+        Video video = videoRepository.findById((long) videoId)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + videoId));
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        int count = playtime / 300;
+        for (int i = 0; i < count; i++) {
+            int maxSize = advertisementRepository.countAllBy();
+            videoAdvertisementRepository.save(VideoAdvertisement.builder()
+                    .videoId(videoId)
+                    .adId(randomNumberGenerator.randomNumberGenerator(maxSize))
+                    .build());
+        }
+    }
+
+}
