25f179ae81ff29f02969f3cb36bef3ffcb10d280
tony <fjzl12@naver.com>
2024-04-30 10:55:53 +0900

    refactor(PlayHistoryRepository): 재생 기록 레포지토리 수정

--- src/main/java/org/example/shortsaccount/repository/PlayHistoryRepository.java
+++ src/main/java/org/example/shortsaccount/repository/PlayHistoryRepository.java
@@ -3,5 +3,9 @@ package org.example.shortsaccount.repository;
 import org.example.shortsaccount.domain.PlayHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayHistoryRepository extends JpaRepository<PlayHistory, Long> {
+    Optional<PlayHistory> findByUserId(int userId);
+    Optional<PlayHistory> findByUserIdAndVideoId(int userId, int videoId);
 }
