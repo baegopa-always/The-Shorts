550ee4287ce39e2a042a66ca56f9206a687bee26
tony <fjzl12@naver.com>
2024-04-30 10:58:54 +0900

    feat(RandomNumberGenerator): 무작위 숫자 생성 기능 구현

---
+++ src/main/java/org/example/shortsaccount/util/RandomNumberGenerator.java
@@ -0,0 +1,9 @@
+package org.example.shortsaccount.util;
+import java.security.SecureRandom;
+
+public class RandomNumberGenerator {
+    public int randomNumberGenerator(int maxSize){
+        SecureRandom secureRandom = new SecureRandom();
+        return secureRandom.nextInt(1,maxSize + 1);
+    }
+}
