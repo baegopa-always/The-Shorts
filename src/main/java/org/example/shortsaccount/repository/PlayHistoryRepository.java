52c3bc6f06ce2cb282f13d611ec7cbf7d6494ecc
tony <fjzl12@naver.com>
2024-05-04 21:58:10 +0900

    feat(PlayHistoryRepository): 재생 기록 레포지토리 수정 - 기간 내 재생 기록 조회 기능 구현 - 사용자 별 최근 재생 기록 조회 구현

--- src/main/java/org/example/shortsaccount/repository/PlayHistoryRepository.java
+++ src/main/java/org/example/shortsaccount/repository/PlayHistoryRepository.java
@@ -3,9 +3,14 @@ package org.example.shortsaccount.repository;
 import org.example.shortsaccount.domain.PlayHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface PlayHistoryRepository extends JpaRepository<PlayHistory, Long> {
     Optional<PlayHistory> findByUserId(int userId);
     Optional<PlayHistory> findByUserIdAndVideoId(int userId, int videoId);
+    Optional<PlayHistory> findFirstByUserIdAndVideoIdOrderByPlayDateDesc(int userId, int videoId);
+    Optional<List<PlayHistory>> findAllByVideoIdAndPlayDateBetween(int videoId, LocalDateTime from, LocalDateTime to);
 }
