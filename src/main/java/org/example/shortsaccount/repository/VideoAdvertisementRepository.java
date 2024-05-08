35467148825feca7ab64bba41842c990b9266b94
tony <fjzl12@naver.com>
2024-05-09 03:51:10 +0900

    feat(VideoAdvertisementRepository): 기간 별 영상 광고 기록 가져오는 기능 구현

--- src/main/java/org/example/shortsaccount/repository/VideoAdvertisementRepository.java
+++ src/main/java/org/example/shortsaccount/repository/VideoAdvertisementRepository.java
@@ -3,7 +3,10 @@ package org.example.shortsaccount.repository;
 import org.example.shortsaccount.domain.VideoAdvertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface VideoAdvertisementRepository extends JpaRepository<VideoAdvertisement, Long> {
+    Optional<List<VideoAdvertisement>> findAllByVideoIdAndAdTimestampBetween(int videoId, LocalDateTime from, LocalDateTime to);
 }
