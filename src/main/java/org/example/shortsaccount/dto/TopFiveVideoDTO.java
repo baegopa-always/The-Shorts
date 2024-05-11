eff007e0e2ebbe93ebe8f42dce6d1ceb3c6f7689
tony <fjzl12@naver.com>
2024-05-12 01:32:06 +0900

    refactor(TopFiveVideoDTO): DTO 리팩터링 - DTO 빌더 추가

--- src/main/java/org/example/shortsaccount/dto/TopFiveVideoDTO.java
+++ src/main/java/org/example/shortsaccount/dto/TopFiveVideoDTO.java
@@ -1,10 +1,12 @@
 package org.example.shortsaccount.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class TopFiveVideoDTO {
     private int videoId;
     private int views;
