af01351d98f9ae37baf4d92b91b6471e13db5c8f
tony <fjzl12@naver.com>
2024-05-12 02:17:12 +0900

    refactor(VideoApiController): 주석 제거

--- src/main/java/org/example/shortsaccount/controller/VideoApiController.java
+++ src/main/java/org/example/shortsaccount/controller/VideoApiController.java
@@ -32,39 +32,30 @@ public class VideoApiController {
     @PostMapping("/api/videos/play/{id}")
     public ResponseEntity<PlayHistory> playVideo(@PathVariable int id, @RequestBody PlayHistoryDTO request) {
         request.setVideoId(id);
-        // video id에 해당하는 video_views count 증가
         videoService.checkVideo(id);
-        // body 에서 user 정보 가져와서 사용자 재생 기록 확인
         PlayHistory playHistory;
         try {
             playHistory = playHistoryService.findFirstByUserIdAndVideoIdOrderByPlayDateDesc(request.getMemberId(), id);
-            // 가장 최근 재생 기록에서 비디오 크기만큼 봤을 경우 0으로 생성해서 반환
             if (playHistory.getLastWatchTime() >= videoService.getLength(id)) {
                 playHistory = playHistoryService.save(request, videoService.getLength(id));
             }
         } catch (IllegalArgumentException e) {
             playHistory = playHistoryService.save(request, videoService.getLength(id));
-            // 0으로 보내주자
         }
-        // 재생 시점 있을 경우 불러와서 재생
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(playHistory);
     }
 
     @PostMapping("/api/videos/stop/{id}")
     public ResponseEntity<PlayHistory> stopVideo(@PathVariable int id, @RequestBody PlayHistoryDTO request) {
-        // play history 생성
         request.setVideoId(id);
-        // 해당 video id에서 재생시간만큼 playback time 증가, ad_views 증가
         int maxSize = videoService.getLength(id);
         if (request.getPlayTime() > maxSize) {
             request.setPlayTime(maxSize);
         }
-
         videoService.addPlayTime(request.getVideoId(), request.getPlayTime());
         videoAdvertisementService.checkVideoAdvertisement(id, request.getPlayTime());
-        // 사용자 기록 업데이트
-        PlayHistory playHistory = playHistoryService.updateLastWatchTime(request); // 여기가 업데이트 였음
+        PlayHistory playHistory = playHistoryService.updateLastWatchTime(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(playHistory);
     }
