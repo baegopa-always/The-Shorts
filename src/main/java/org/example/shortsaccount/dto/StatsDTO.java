e478d34e51845f23a3f86ab4f58ac553a79e26fd
tony <fjzl12@naver.com>
2024-05-12 01:31:40 +0900

    refactor(StatsDTO): DTO 리팩터링 - DTO 빌더 추가

--- src/main/java/org/example/shortsaccount/dto/StatsDTO.java
+++ src/main/java/org/example/shortsaccount/dto/StatsDTO.java
@@ -1,5 +1,6 @@
 package org.example.shortsaccount.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Builder
 public class StatsDTO {
     private int videoId;
     private int statsAmount;
